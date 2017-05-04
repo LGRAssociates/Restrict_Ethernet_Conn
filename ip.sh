fcf407a01d13d5dbeca8ffb5a3283da5857e414e
LGRAssociates <larrygrobertson@netzero.net>
2017-05-04 09:14:40 -0400

    Update ip.sh
    Changed echo statements to be more generic.

--- ip.sh
+++ ip.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
-APS=1
-while [ APS=1 ]; do
+WORK_LOOP=1
+while [ WORK_LOOP=1 ]; do
     ifconfig | grep "inet" | grep -Fv 127.0.0.1 |awk '{print $2}' > ip.txt
     File=ip.txt  
-    if grep -q 10.20.2 "$File"; ##note the space after the string you are searching for
+    if grep -q 10.20.2 "$File"; ## searching for partial ip address of unwanted network
     then
     	lsof -i | grep -E "(LISTEN|ESTABLISHED)" > port.txt
     	File2=port.txt  
-    	if grep -q :4433 "$File2"; ##note the space after the string you are searching for
+    	if grep -q :4433 "$File2"; ## searching for Sonic Wall Port Number
     	then
-        	echo "Sonic Wall"
+        	echo "Sonic Wall: It's okay to connect to unwanted network through the Sonic Wall"
     	else
-	        echo "Oops!! Not APS"
+	        echo "Oops! unwanted network"
        		echo "Not Sonic Wall"
 	        #reboot here
-    	    sudo shutdown -h now
-   		fi
+    	    	sudo shutdown -h now
+   	fi
     else
-       echo "Hooray!!It's APS"
+       echo "Hooray! It's a welcome network"
    fi
 done
 
